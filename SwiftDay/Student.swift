752ffeec1d3505ff8c02cca1420120f6c325d959
MacStudent <macstudent@126-imac29.local>
2019-02-14 19:42:33 -0500

    first commit in swiftday

---
+++ SwiftDay/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  SwiftDay
+//
+//  Created by MacStudent on 2019-02-14.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Student
+{
+    //jubin ks
+}
